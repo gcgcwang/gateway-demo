d5b63fbec643d6bfa2002459bf3c518455f0ce7a
wangguochao-001 <wangguochao-001@cpic.com.cn>
2020-08-27 15:53:47 +0800

    Initial commit

---
+++ src/main/java/com/cpic/gatewaydemo/GatewayDemoApplication.java
@@ -0,0 +1,14 @@
+package com.cpic.gatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class GatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayDemoApplication.class, args);
+	}
+
+}
